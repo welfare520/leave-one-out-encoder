84225271d221b155df36a71153a1168ffa58ed20
He Zhang <zhanghe.dr@gmail.com>
2018-07-15 00:38:07 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='leave-one-out-encoder',
+      version='0.0.1',
+      description='Leave one out encoding of categorical features',
+      url='https://github.com/welfare520/leave-one-out-encoder',
+      author='He Zhang',
+      author_email='zhanghe.dr@gmail.com',
+      license='GPL v3',
+      packages=['leave-one-out-encoder'],
+      zip_safe=False)
