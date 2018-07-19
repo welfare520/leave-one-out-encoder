7a4b22250253d59c013810adbb773397ec7d85c4
He Zhang <zhanghe.dr@gmail.com>
2018-07-19 10:58:23 +0200

    add setup

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@
 
 from setuptools import setup
 
-setup(name='leave-one-out-encoder',
-      version='0.0.1',
+setup(name='loo_encoder',
+      version='0.0.3',
       description='Leave one out encoding of categorical features',
       url='https://github.com/welfare520/leave-one-out-encoder',
       author='He Zhang',
       author_email='zhanghe.dr@gmail.com',
       license='GPL v3',
-      packages=['leave-one-out-encoder'],
+      packages=['loo_encoder'],
+      install_requires=[
+          'pandas', 'numpy'
+      ],
       zip_safe=False)
